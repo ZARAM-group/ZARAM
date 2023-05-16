d4a6f5589de53e4f398eb5c40738a104a4b11f02
RayenTellissy <rayentellissy571@gmail.com>
2023-05-16 23:31:24 +0100

    added login and sign up on the backend

--- backend/controller/user.ts
+++ backend/controller/user.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import User from "../model/user";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 export default {
     getAll:(req: Request, res: Response)=> {
@@ -16,6 +18,50 @@ export default {
         const{UserId,itemId}=req.params
         
 
+    },
+
+    signup: async (req: Request, res: Response)=>{
+        const { user, pass, fName, lName, email } = req.body
+        const existingUser=await User.find({username: user})
+        if(existingUser){
+            res.send({message: "Username Already Exists"})
+        }
+        else{
+            const hashedPassword = await bcrypt.hash(pass,10)
+            const newUser={
+                username: user,
+                password: hashedPassword,
+                firstName: fName,
+                lastName: lName,
+                email: email,
+                isAdmin: false,
+                cart: []
+            }
+            User.create(newUser)
+        }
+    },
+
+    login: async (req: Request, res: Response)=>{
+        const { user, pass } = req.body
+        const loggedUser = await User.find({username: user})
+        if(!loggedUser){
+            res.send({message: "User Doesn't Exist"})
+        }
+        else{
+            if(await bcrypt.compare(pass,(loggedUser as any).password)){
+                res.send({message: "Password Incorrect"})
+            }
+            else{
+                const token = await jwt.sign({id: (loggedUser as any)._id},process.env.token)
+                res.send({
+                    token: token,
+                    username: user,
+                    fName: (loggedUser as any).fName,
+                    lName: (loggedUser as any).lName,
+                    email: (loggedUser as any).email
+                })
+            }
+        }
     }
 }
 
