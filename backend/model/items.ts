d04de8a6e31b40c1d6f8eccc4ea3ba4c56e0babc
RayenTellissy <rayentellissy571@gmail.com>
2023-05-16 11:29:11 +0100

    progress

---
+++ backend/model/items.ts
@@ -0,0 +1,27 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface Item extends Document {
+  name: string;
+  price: number;
+  color: string;
+  type: string;
+  size: string;
+  gender: string;
+  description: string;
+  conditions: string[];
+}
+
+const itemSchema: Schema = new Schema({
+  name: String,
+  price: Number,
+  color: String,
+  type: String,
+  size: String,
+  gender: String,
+  description: String,
+  conditions: [String],
+});
+
+const ItemModel = mongoose.model<Item>('items', itemSchema);
+
+export default ItemModel;
