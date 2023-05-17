4840f91f0831301197687a1e23654dc324144060
Mohamed-Aziz-Belarbi <aziz.belarbi1998@gmail.com>
2023-05-17 17:33:31 +0100

    .

--- backend/routes/user.ts
+++ backend/routes/user.ts
@@ -5,7 +5,6 @@ import authenticate from '../middleware/authenticate';
 const router: Router = express.Router();
 
 router.get('/getAll', User.getAll);
-router.post('/add',User.addToCart);
 router.post("/login",User.login)
 router.post("/signup", User.signup)
 router.post('/add', authenticate, User.addToCart);
