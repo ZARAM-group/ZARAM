da59e58c8534ca096ef7c10ac5dc551325c044ca
Mohamed-Aziz-Belarbi <aziz.belarbi1998@gmail.com>
2023-05-18 17:39:35 +0100

    .

--- frontend/components/Offcanvas/Offcanvas.tsx
+++ frontend/components/Offcanvas/Offcanvas.tsx
@@ -1,6 +1,5 @@
 "use client"
 import { useState } from "react";
-import Link from "next/link"
 import WomanItems from "./womanItems";
 import ManItems from "./manItems";
 import KidsItems from "./kidsItems";
